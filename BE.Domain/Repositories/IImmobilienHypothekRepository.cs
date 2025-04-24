1dc3cc74c8dd6c0dc977c507d14b0bdb3e160cef
lwu <lukewulfestieg@gmx.de>
2025-04-24 22:44:24 +0200

    add Repository Setup for Hypothekenrechnung

---
+++ BE.Domain/Repositories/IImmobilienHypothekRepository.cs
@@ -0,0 +1,13 @@
+﻿using BE.Domain.Entities.Hypothek;
+
+namespace BE.Domain.Repositories
+{
+    public interface IImmobilienHypothekRepository
+    {
+        Task<IEnumerable<ImmobilienHypothek>> GetAllAsync();
+        Task<ImmobilienHypothek?> GetByIdAsync(int id);
+        Task<int> Create(ImmobilienHypothek entity);
+        Task Delete(ImmobilienHypothek entity);
+        Task SaveChanges();
+    }
+}
